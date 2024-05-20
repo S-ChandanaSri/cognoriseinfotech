558cb9413bbe6cf5e257b31b96dee01902bd1132
S-ChandanaSri <chandanaofficial8@gmail.com>
2024-05-20 16:10:41 +0530

    making myfolder available

--- cognorise/todo/todoo/src/components/Box1.js
+++ cognorise/todo/todoo/src/components/Box1.js
@@ -25,33 +25,18 @@ const Eedit = ({ task, index, handleSave }) => {
 
   return (
     <div>
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target={`#modal-${index}`}
-      >
+      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#modal-${index}`}>
         EDIT
       </button>
 
       <div
-        className="modal fade"
-        id={`modal-${index}`}
-        data-bs-backdrop="static"
-        data-bs-keyboard="false"
-        tabIndex="-1"
-        aria-labelledby="staticBackdropLabel"
-        aria-hidden="true"
+        className="modal fade" id={`modal-${index}`} data-bs-backdrop="static"  data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true"
       >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
               <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
+                type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" ></button>
             </div>
             <div className="modal-body">
               TASK:
